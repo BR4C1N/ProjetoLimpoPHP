b950da1cdad99f490f6ecaa9c4693eeb5da11b8c
Matheus Moreira Mendes <802.238@alunos.unigran.br>
2022-11-15 17:47:30 -0400

    Correçõs no Index

--- root/source/src/index.php
+++ root/source/src/index.php
@@ -1,2 +1,8 @@
 <?php
-    phpinfo();
+    namespace App\Model;
+
+    require_once __DIR__ . "/model/Pergunta.php";
+
+    $pergunta = new Pergunta("teste", "done");
+
+    echo($pergunta->getPergunta());
